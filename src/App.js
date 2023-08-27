5634ff500cc0327274f9fb9ce138969490fb6a32
Aakash-Developer <aakash20899macwan@gmail.com>
2023-08-28 03:17:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import RootRouter from "./routers/RootRouter";
 import "animate.css";
 import { Provider } from "react-redux";
 import Store from "./store/store";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
+  const [pageLoaded, setPageLoaded] = useState(false);
+  useEffect(() => {
+    window.onload = () => {
+      setPageLoaded(true);
+    };
+  }, []);
+  return <div>{!pageLoaded ? <div className="loader">Loading...</div> : <Main />}</div>;
+}
+
+function Main() {
   return (
     <Provider store={Store}>
       <RootRouter />
